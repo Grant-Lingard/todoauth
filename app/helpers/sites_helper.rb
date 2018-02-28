f8206ce762d8be13f596907deb27502e88f9fea0
Christopher Slade <crslade@gmail.com>
2018-02-28 03:23:17 +0000

    Add Navdrawer

--- app/helpers/sites_helper.rb
+++ app/helpers/sites_helper.rb
@@ -1,21 +1,31 @@
 module SitesHelper
     def title(page_title, show_title=true)
         content_for(:title) { page_title.to_s }
-        @show_title = show_title
-    end
+        @show_title = show_title 
+    end 
     
+    #Don't use this. This works with Material
     def nav_link(text,path)
-       if current_page?(path)
-           content_tag :li, class: 'nav-item active' do
-               link_to(path, class: 'nav-link') do
-                   sanitize(text) + content_tag(:span, "(current)", class: "sr-only")
-               end
-           end
+        if current_page?(path)
+            link_to(text,path, class: ["nav-item","nav-link","active"])
         else
-            content_tag :li, class: 'nav-item' do
-                link_to(text, path, class: 'nav-link')
-            end
+            link_to(text,path, class: ["nav-item","nav-link"])
         end
     end
+    
+    #Use this example for Bootstrap only.
+    #def nav_link(text,path)
+    #   if current_page?(path) 
+    #       content_tag :li, class: 'nav-item active' do
+    #           link_to(path, class: 'nav-link') do
+    #               sanitize(text) + content_tag(:span, "(current)", class: "sr-only") 
+    #           end
+    #       end 
+    #    else 
+    #        content_tag :li, class: 'nav-item' do
+    #            link_to(text, path, class: 'nav-link') 
+    #        end   
+    #    end
+    #end
 
 end
